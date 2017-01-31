8aeb42aaedc7b31d001c02c3bca16776e8e010da
Nara Hahn <nara.hahn@uni-rostock.de>
2017-01-31 18:47:55 +0100

    make examples for virtual point sources and plane waves

--- examples/time_domain_nfchoa.py
+++ examples/time_domain_nfchoa.py
@@ -17,28 +17,32 @@ N = 60  # number of secondary sources
 R = 1.5  # radius of spherical/circular array
 x0, n0, a0 = sfs.array.circular(N, R)  # get secondary source positions
 
-# dirac impulse
+# Signals
 L = 1024
+
 dirac = np.zeros(L)
 dirac[0] = 1
+
 f0 = 2333
 time = np.linspace(0,L,num=L)*(1/fs)
-#harm = (1/4)*np.exp(1j*2*np.pi*f0*time)
 harm = (1/4)*np.cos(2*np.pi*f0*time)
+
 signal = dirac
 
+# NFC-HOA setup
 max_order = None
 max_order = sfs.mono.drivingfunction._max_order_circular_harmonics(N, max_order)
 
 # POINT SOURCE
-xs = [1.5, 1.5, 0]  # position of virtual source
-t = (np.sqrt(2)*1.5)/sfs.defs.c
-delay, weight, sos, phaseshift = sfs.time.drivingfunction.nfchoa_25d_point(x0,R,xs,max_order=max_order,normalize=True)
+#xs = [1.5, 1.5, 0]  # position of virtual source
+#t = (np.sqrt(2)*1.5)/sfs.defs.c
+#delay, weight, sos, phaseshift = sfs.time.drivingfunction.nfchoa_25d_point(x0,R,xs,max_order=max_order,normalize=True)
 
 # PLANE WAVE
-#npw = [0,-1,0]
-#t = 0/sfs.defs.c
-#delay, weight, sos, phaseshift = sfs.time.drivingfunction.nfchoa_25d_plane(x0,R,npw,max_order=max_order)
+npw = [0,-1,0]  
+t = 0/sfs.defs.c
+delay, weight, sos, phaseshift = sfs.time.drivingfunction.nfchoa_25d_plane(x0,R,npw,max_order=max_order, normalize=True)
+t = 0
 
 #t += L/fs/2
 
@@ -58,6 +62,7 @@ plt.colorbar()
 plt.clim([-0.1,0.1])
 
 t -= t_offset
+scale = 1
 
 # Synthesized sound field
 a0 = np.ones(len(x0))
@@ -67,20 +72,22 @@ plt.figure(figsize=(4, 4))
 im = sfs.plot.level(p, grid, cmap='Blues')
 sfs.plot.loudspeaker_2d(x0, n0)
 plt.grid()
-sfs.plot.virtualsource_2d(xs)
+sfs.plot.virtualsource_2d(npw)
+sfs.plot.virtualsource_2d([0,1], npw, type='plane')
 #plt.title('impulse_ps_nfchoa_25d')
 #plt.savefig('impulse_ps_nfchoa_25d.eps')
 #plt.savefig('impulse_ps_nfchoa_25d.png')
 
 plt.figure(figsize=(4, 4))
-sfs.plot.soundfield(p*20, grid, cmap='coolwarm')
+sfs.plot.soundfield(scale*p, grid, cmap='coolwarm')
 sfs.plot.loudspeaker_2d(x0, n0)
 plt.grid()
 plt.title('$M=%d$'%max_order)
-sfs.plot.virtualsource_2d(xs)
+#sfs.plot.virtualsource_2d(npw)
+sfs.plot.virtualsource_2d([0, 1], npw, type='plane')
 #plt.title('impulse_ps_nfchoa_25d')
 #plt.savefig('impulse_ps_nfchoa_25d_M%d.eps'%(max_order), bbox_inches='tight')
 #plt.savefig('impulse_ps_nfchoa_25d_M%d.png'%(max_order), bbox_inches='tight')
-plt.savefig('impulse_ps_nfchoa_25d_M%d.eps'%(max_order), bbox_inches='tight')
-plt.savefig('impulse_ps_nfchoa_25d_M%d.png'%(max_order), bbox_inches='tight')
+#plt.savefig('impulse_pw_nfchoa_25d_M%d.eps'%(max_order), bbox_inches='tight')
+#plt.savefig('impulse_pw_nfchoa_25d_M%d.png'%(max_order), bbox_inches='tight')
 
