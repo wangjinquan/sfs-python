d782905123b2ab3ea6e6488f24e03d0c84c4a176
Nara Hahn <n.hahn343@gmail.com>
2018-12-31 01:32:33 +0100

    Slight change of sfs.plot.particles

--- sfs/plot.py
+++ sfs/plot.py
@@ -338,13 +338,11 @@ def particles(x, trim=None, ax=None, xlabel='x (m)', ylabel='y (m)',
     if ax is None:
         ax = plt.gca()
 
-    scat = ax.scatter(XX, YY, edgecolor=edgecolor, **kwargs)
-
     if xlabel:
         ax.set_xlabel(xlabel)
     if ylabel:
         ax.set_ylabel(ylabel)
-    return scat
+    return ax.scatter(XX, YY, edgecolor=edgecolor, **kwargs)
 
 
 def vectors(v, grid, cmap='blacktransparent', headlength=3, headaxislength=2.5,
